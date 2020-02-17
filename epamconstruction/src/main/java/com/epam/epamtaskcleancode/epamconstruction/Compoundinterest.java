c23dc6c3164348ed44c53bdc38f649befd82d1d5
ushanagasri <ushanagasri2000@gmail.com>
2020-02-17 21:14:54 +0530

    first commit

---
+++ epamconstruction/src/main/java/com/epam/epamtaskcleancode/epamconstruction/Compoundinterest.java
@@ -0,0 +1,20 @@
+package com.epam.epamtaskcleancode.epamconstruction;
+
+public class Compoundinterest {
+	
+	     public double initialbalance;
+	     public double rateofinterest;
+	    public int nooftimeperiods,numberoftimes;
+	     Compoundinterest(){
+	    	 this.initialbalance=1000.0;
+	    	 this.rateofinterest=3.5;
+	    	 this.nooftimeperiods=2;
+	    	 this.numberoftimes=3;
+	     }
+	     public double getinterest() {
+	    	 return initialbalance*Math.pow((1+rateofinterest/numberoftimes),numberoftimes*nooftimeperiods);
+
+	     }
+	}
+
+
